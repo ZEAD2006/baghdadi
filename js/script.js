9b4a3453c348b7592c108fbe8c0afc06b2e3d6b6
ZEAD2006 <zeadnasser39@gmail.com>
2024-10-25 09:31:39 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*typing animations */
+var typed = new Typed(".typing", {
+    strings: ["", "web developer", "web designer", "front-end developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
