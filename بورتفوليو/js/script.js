de9f102cb5cd640519b82394c4109c0957fa3707
Baghdadi <zeadnasser39@gmail.com>
2024-10-25 09:51:35 +0200

    Delete بورتفوليو directory

--- بورتفوليو/js/script.js
+++
@@ -1,7 +0,0 @@
-/*typing animations */
-var typed = new Typed(".typing", {
-    strings: ["", "web developer", "web designer", "front-end developer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
